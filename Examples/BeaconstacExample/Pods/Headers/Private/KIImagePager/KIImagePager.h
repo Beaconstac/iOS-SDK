87142f79e8b6feb1ec80fac3b56cce1ad90ca854
Sachin Vas <sachin@mobstac.com>
2018-01-03 14:33:23 +0530

    Clean up 1.5.3 related objects

--- Examples/BeaconstacExample/Pods/Headers/Private/KIImagePager/KIImagePager.h
+++
@@ -1,1 +0,0 @@
-../../../KIImagePager/KIImagePager/KIImagePager/KIImagePager.h
--- Examples/BeaconstacExample/Pods/Target Support Files/Pods-BeaconstacExample/Pods-BeaconstacExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BeaconstacExample : NSObject
-@end
-@implementation PodsDummy_Pods_BeaconstacExample
-@end
